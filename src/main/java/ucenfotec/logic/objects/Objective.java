3e938db8dc5a9098cdb9a07f4d99849476213ef2
Jorge Gómez <jgomezm@ucenfotec.ac.cr>
2018-04-12 20:24:55 -0600

    Objective class
    Created Objective class

--- src/main/java/ucenfotec/logic/objects/Objective.java
+++ src/main/java/ucenfotec/logic/objects/Objective.java
@@ -1,5 +1,37 @@
 package ucenfotec.logic.objects;
 
 public class Objective {
+	
+	private String name;
+	private boolean status;
+	
+	public Objective(String pName, boolean pStatus) {
+		this.setName(pName);
+		this.setStatus(pStatus);
+	}
+	
+	public Objective() {}
 
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String pName) {
+		this.name = pName;
+	}
+
+	public boolean isStatus() {
+		return status;
+	}
+
+	public void setStatus(boolean pStatus) {
+		this.status = pStatus;
+	}
+
+	@Override
+	public String toString() {
+		return "Objective [name=" + name + ", status=" + status + "]";
+	}
+	
+	
 }
