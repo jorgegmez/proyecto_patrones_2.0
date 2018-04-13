3da48095b21f966bc4691e485f47ec638c9a0123
Jorge Gómez <jgomezm@ucenfotec.ac.cr>
2018-04-12 20:59:41 -0600

    AccessDB class
    Created AccessDB class

--- src/main/java/ucenfotec/logic/dbaccess/AccessDB.java
+++ src/main/java/ucenfotec/logic/dbaccess/AccessDB.java
@@ -1,5 +1,69 @@
 package ucenfotec.logic.dbaccess;
 
-public class AccessDB {
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
+public class AccessDB {
+	private Connection conn = null;
+	private Statement st;
+	
+
+	public AccesoDB(String driver,String conexion) throws SQLException{
+		conn = DriverManager.getConnection(conexion);
+		st = conn.createStatement();
+	}
+	public PreparedStatement getPreparedStatement(String sql) throws SQLException{
+		return conn.prepareStatement(sql);
+    }
+        
+    public CallableStatement getCallableStatement(String sql) throws SQLException{
+        return conn.prepareCall(sql);
+    }
+	
+	public void ejecutarSQL(String sentencia) 
+	throws SQLException{	
+		st.execute(sentencia);		
+	}
+	
+	public ResultSet ejecutarSQL(String sentencia,
+								boolean retorno) throws SQLException{
+		ResultSet rs;
+		rs = st.executeQuery(sentencia);
+		return rs;
+	}
+	
+	public void iniciarTransaccion()
+	throws java.sql.SQLException{
+		conn.setAutoCommit(false);	
+	}
+	
+	public void terminarTransaccion() throws java.sql.SQLException{
+		conn.setAutoCommit(true);
+	}
+		
+	public void aceptarTransaccion() throws java.sql.SQLException{
+		conn.commit();
+	}
+
+	public void deshacerTransaccion() throws java.sql.SQLException{
+		conn.rollback();	
+	}
+
+	protected void finalize(){
+		try {
+			conn.close();
+                       
+		}
+		catch(SQLException e){
+			e.getErrorCode();
+			e.getSQLState();
+			e.getCause();
+		}
+               
+	}
 }
